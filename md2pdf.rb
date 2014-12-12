8f1adf9da954c19b6810882ce5bddaaca579b1be
cagayakemiracl <hirosnonno1182@gmail.com>
2014-12-12 23:10:23 +0900

    cssファイルを指定できるようにした

--- md2pdf.rb
+++ md2pdf.rb
@@ -1,15 +1,26 @@
 #!/usr/bin/env ruby
 
 require 'pathname'
+require 'optparse'
+
+params = ARGV.getopts(
+  '',
+  'css:github',
+  'input:not_found_file')
+
+exit 0 if params['input'] == 'not_found_file'
 
 this_dir = File.dirname Pathname.new(__FILE__).realpath
+if params['css'] == 'github'
+  style = File.expand_path 'github2.css', this_dir
+else
+  style = params['css']
+end
 
-markdown_file = ARGV.shift
+markdown_file = params['input']
 basename = Pathname(markdown_file)
 html_file = basename.sub_ext('.html').to_s
 pdf_file = basename.sub_ext('.pdf').to_s
-
-style = File.expand_path 'github2.css', this_dir
 
 delay_time = 25_000
 wkhtmltopdf_option = "--javascript-delay #{delay_time} --footer-center [page]"
