b00ddf76a3230b2cfd1bd6a3844fac14cb583854
cagayakemiracl <hirosnonno1182@gmail.com>
2014-12-12 22:58:00 +0900

    githubのmarkdown用cssを追加

--- md2pdf.rb
+++ md2pdf.rb
@@ -1,12 +1,18 @@
 #!/usr/bin/env ruby
 
+require 'pathname'
+
+this_dir = File.dirname Pathname.new(__FILE__).realpath
+
 markdown_file = ARGV.shift
-basename = File.basename markdown_file, '.md'
-html_file = basename + '.html'
-pdf_file = basename + '.pdf'
-style = '~/style/GitHub2.css'
+basename = Pathname(markdown_file)
+html_file = basename.sub_ext('.html').to_s
+pdf_file = basename.sub_ext('.pdf').to_s
+
+style = File.expand_path 'github2.css', this_dir
+
 delay_time = 25_000
-wkhtmltopdf_option = "--javascript-delay #{delay_time} --footer-center [page] --page-size A4"
+wkhtmltopdf_option = "--javascript-delay #{delay_time} --footer-center [page]"
 
 system "pandoc #{markdown_file} --mathjax -s -c #{style} -o #{html_file}"
 system "wkhtmltopdf #{wkhtmltopdf_option} #{html_file} #{pdf_file}"
